470a570d5db69ff8a7b1614e18e8e482aa6cc136
Dmitriev Andrey <dmitrievandryha94@gmail.com>
2020-10-06 09:05:57 +0300

    add get articles by topic

--- src/main/java/org/blog/controller/ArticleController.java
+++ src/main/java/org/blog/controller/ArticleController.java
@@ -1,25 +1,41 @@
 package org.blog.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.blog.dao.ArticleDao;
+import org.blog.dao.TopicDao;
+import org.blog.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping("/articles")
 public class ArticleController {
 
     private ArticleDao articleDao;
+    private TopicDao topicDao;
 
     @Autowired
-    public ArticleController(ArticleDao articleDao) {
+    public ArticleController(ArticleDao articleDao, TopicDao topicDao) {
         this.articleDao = articleDao;
+        this.topicDao = topicDao;
     }
 
-    @GetMapping("/getAllArticle")
-    public String getArticle() {
-        StringBuilder builder = new StringBuilder();
-        articleDao.findAll()
-                .forEach(article -> builder.append(article.toString()));
-        return builder.toString();
+    @GetMapping("/{topicId}")
+    public String getArticles(@PathVariable Long topicId) throws JsonProcessingException {
+        Map<Long, String> articleNames = topicDao.findById(topicId)
+                .map(topic -> articleDao.findAllByTopic(topic))
+                .map(articles -> articles.stream()
+                        .collect(Collectors.toMap(Article::getId, Article::getTitle)))
+                .orElse(new HashMap<>());
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(articleNames);
     }
 }
--- src/main/java/org/blog/dao/ArticleDao.java
+++ src/main/java/org/blog/dao/ArticleDao.java
@@ -1,9 +1,13 @@
 package org.blog.dao;
 
 import org.blog.model.Article;
+import org.blog.model.Topic;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 public interface ArticleDao extends CrudRepository<Article, Long> {
+    List<Article> findAllByTopic(Topic topic);
 }
